280a58bc614a0c9ff8de835b792e91d03ea26f7f
Saeed Rostami <privatemx.2010@gmail.com>
2026-09-19 23:01:45 +0330

    bootstrap Go module and minimal CLI entrypoint

---
+++ cmd/prolane/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Prolane\nProve every software change before it reaches production.")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/prolane
+
+go 1.27.1
